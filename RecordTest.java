baacae12de4aeaa6d863045dce18b5050385b38f
HTML93 <stewartphippsgoodwin@gmail.com>
2025-11-06 11:12:50 -0500

    ITS WORKING!!!!!!!!!

--- RecordTest.java
+++ RecordTest.java
@@ -8,6 +8,7 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.Line;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.Port;
 import javax.sound.sampled.TargetDataLine;
@@ -17,13 +18,14 @@ import java.util.ArrayList;
 public class RecordTest {
 
     public static void main(String[] args) {
-        try {
+       try {
 
-            AudioFormat audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false);
+            AudioFormat audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 24000, 16, 1, 2, 48000, false);
             DataLine.Info dataInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
             if (!AudioSystem.isLineSupported(dataInfo)) {
                 System.out.println("Not Sup");
             }
+
 
             TargetDataLine targetLine;
             Mixer myMixer;
@@ -37,18 +39,23 @@ public class RecordTest {
                 myMixer = AudioSystem.getMixer(mixerInfo[i]);
 
                 if (myMixer.isLineSupported(Port.Info.MICROPHONE)) {
-                    System.out.println("Mic is supported!");
+                    System.out.println("Mic is supported");
                     mixerArray.add(myMixer);
                 }
 
             }
-            // miced=mixerArray.get(0);
+            /*System.out.println(miced);
+            miced = mixerArray.get(0);
+            System.out.println(miced);
+            if (miced != null) {
+                targetLine = (TargetDataLine) miced.getLine(dataInfo);
 
-
-
+            } else {
+                System.out.println("No Mic Detected"); */
                 targetLine = (TargetDataLine) AudioSystem.getLine(dataInfo);
 
-
+            System.out.println(targetLine);
+           
             targetLine.open();
 
             JOptionPane.showMessageDialog(null, "hit ok to record");
@@ -72,7 +79,48 @@ public class RecordTest {
             targetLine.close();
         } catch (Exception e) {
             System.out.println(e);
-        }
+        } 
+
     }
 
+public static void displayMixerInfo()
+{
+  Mixer.Info [] mixersInfo = AudioSystem.getMixerInfo();
+
+  for (Mixer.Info mixerInfo : mixersInfo)
+   {
+     System.out.println("Mixer: " + mixerInfo.getName());
+
+     Mixer mixer = AudioSystem.getMixer(mixerInfo);
+
+     Line.Info [] sourceLineInfo = mixer.getSourceLineInfo();
+     for (Line.Info info : sourceLineInfo)
+      {
+        showLineInfo(info);
+      }
+
+     Line.Info [] targetLineInfo = mixer.getTargetLineInfo();
+     for (Line.Info info : targetLineInfo)
+      {
+        showLineInfo(info);
+      }
+   }
+}
+
+
+private static void showLineInfo(Line.Info lineInfo)
+{
+  System.out.println("  " + lineInfo.toString());
+
+  if (lineInfo instanceof DataLine.Info)
+   {
+     DataLine.Info dataLineInfo = (DataLine.Info)lineInfo;
+
+     AudioFormat [] formats = dataLineInfo.getFormats();
+     for (AudioFormat format : formats)
+      {
+        System.out.println("    " + format.toString());
+      }
+   }
+}
 }
