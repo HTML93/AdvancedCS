210bda1f00ed9601ae4e01b20a3342a5805b2195
HTML93 <stewartphippsgoodwin@gmail.com>
2026-01-07 19:09:56 -0500

    testarea

---
+++ testArea.java
@@ -0,0 +1,31 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.sound.sampled.Line;
+import javax.swing.*;
+public class testArea extends JFrame {
+    testArea() {
+        setTitle("Test Area");
+        setLayout(new FlowLayout());
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        add(new navBar());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new testArea();
+    }
+}
